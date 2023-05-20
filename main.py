90c63d66ae4977c072131e3c5d828ac3697f74dd
Elie Sfeir <42282499+Internalizable@users.noreply.github.com>
2023-05-20 16:47:17 +0300

    Configure CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 from models.plant import Plant
@@ -11,6 +12,14 @@ from ai.aimodel import AIModel
 from dependencies.dependencies import get_socket_manager
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(plants.router, dependencies=[Depends(get_socket_manager)])
 app.include_router(users.router)
@@ -25,6 +34,7 @@ async def startup_event():
     asyncio.create_task(client.connect("test.mosquitto.org"))
 
     await engine.configure_database([User, Plant])
+
 
 @app.get("/")
 async def root():
